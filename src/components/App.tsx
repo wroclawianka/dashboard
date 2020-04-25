8df9c5d245e16449d6dadd81813262e994361a8d
Dorota Zelga <dorota.zelga@gmail.com>
2020-04-25 18:30:27 +0200

    waether widget

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Weather from './widgets/weather';
 
-function App() {
+const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Weather/>
+    </div>
   );
 }
 
