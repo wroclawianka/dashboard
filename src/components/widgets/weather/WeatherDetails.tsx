beecc019a7298df5f4f7296fd6cdabc14d691650
Dorota Zelga <dorota.zelga@gmail.com>
2020-05-04 20:35:48 +0200

    weather widget - stlying

--- src/components/widgets/weather/WeatherDetails.tsx
+++ src/components/widgets/weather/WeatherDetails.tsx
@@ -9,7 +9,7 @@ import Typography from '@material-ui/core/Typography';
 const useStyles = makeStyles(() =>
     createStyles({
         root: {
-            color: '#02005c'
+            color: '#02005c',
         },
         title: {
             textAlign: 'left',
@@ -28,6 +28,11 @@ const useStyles = makeStyles(() =>
         },
         verticallyCentered: {
             margin: 'auto 0',
+            textAlign: 'center',
+        },
+        feelsLike: {
+            fontSize: '0.70rem',
+            lineHeight: '1.5',
         }
     }),
 );
@@ -38,56 +43,51 @@ const WeatherDetails = () => {
 
     if (weather) {
         return (
-            <Box className={classes.root}>
-                <Grid container spacing={0} justify="space-evenly">
-                    <Grid item container justify="space-evenly" xs={12}>
-                        <Grid item xs={7}>
-                            <Grid item className={classes.title}>
+            <div>
+                <Box className={classes.root}>
+                    <Grid container spacing={0} direction="column">
+                        <Grid container justify="space-between" direction="row">
+                            <Grid className={classes.title}>
                                 <Typography variant="h5">
                                     {weather.main.location}
-                                </Typography>
-                                <Typography variant="subtitle1">
-                                    {weather.main.description}
-                                </Typography>
-                            </Grid>
-                            <Grid item className={classes.mainInfo} container direction="row">
-                                <div>
-                                    <img src={`./assets/weather_icons/${weather.main.icon}.svg`} width='100px' alt="Sun" />
-                                </div>
-                                <div className={classes.verticallyCentered}>
-                                    <Typography variant="h4">
-                                        {`${weather.main.temp} °C`}
+                                </Typography></Grid>
+                            <Grid>------- INPUT ------</Grid>
+                        </Grid>
+                        <Grid container justify="space-evenly" direction="row">
+                            <Grid className={classes.mainInfo}>
+                                <Grid>
+                                    <Typography variant="subtitle1">
+                                        {weather.main.description}
                                     </Typography>
-                                </div>
+                                </Grid>
+                                <Grid container justify="space-evenly" direction="row">
+                                    <Grid>
+                                        <div>
+                                            <img src={`./assets/weather_icons/${weather.main.icon}.svg`} width='100px' alt="Sun" />
+                                        </div>
+                                    </Grid>
+                                    <Grid className={classes.verticallyCentered}>
+                                        <Typography variant="h4">
+                                            {`${weather.main.temp} °C`}
+                                        </Typography>
+                                        <Typography variant="overline" display="block" gutterBottom className={classes.feelsLike}>
+                                            <div>Feels like</div>
+                                            <div>{`${weather.temperature.feelsLike} °C`}</div>
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                            <Grid className={classes.detailInfo}>
+                                <Typography variant="subtitle1">
+                                    <p>Humidity:{`${weather.details.humidity}%`}</p>
+                                    <p>Cloudness: {`${weather.details.cloudiness}%`}</p>
+                                    <p>Wind: {`${weather.details.wind} m/sec`}</p>
+                                </Typography>
                             </Grid>
                         </Grid>
-                        <Grid item xs={5} className={classes.detailInfo}>
-                            <Typography variant="subtitle1">
-                                <p>{`Humidity: ${weather.details.humidity}%`}</p>
-                                <p>{`Cloudness: ${weather.details.cloudiness}%`}</p>
-                                <p>{`Wind: ${weather.details.wind} m/sec`}</p>
-                            </Typography>
-                        </Grid>
                     </Grid>
-                    <Grid item container direction="row" justify="space-evenly" className={classes.tempDetails}>
-                        <Grid>
-                            <Typography variant="overline" display="block" gutterBottom>
-                                {`Min ${weather.temperature.min} °C`}
-                            </Typography>
-                        </Grid>
-                        <Grid>
-                            <Typography variant="overline" display="block" gutterBottom>
-                                {`Max ${weather.temperature.max} °C`}
-                            </Typography>
-                        </Grid>
-                        <Grid>
-                            <Typography variant="overline" display="block" gutterBottom>
-                                {`Feels like ${weather.temperature.feelsLike} °C`}
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Box>
+                </Box>
+            </div>
         );
     } else {
         return (
