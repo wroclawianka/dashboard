bf3e6573fe905e16e0adb55db15a610d798f8e00
Dorota Zelga <dorota.zelga@gmail.com>
2020-05-04 21:29:43 +0200

    Updates

---
+++ precache-manifest.f43fa881b8904db9d3cf9b18aeefd0c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835d2ba7fd505a9e8092f4561670793e",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "f45a18bcfd4d63388396",
+    "url": "/dashboard/static/css/main.d8ec1095.chunk.css"
+  },
+  {
+    "revision": "a82583866cc169fd43f1",
+    "url": "/dashboard/static/js/2.2a4659e3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dashboard/static/js/2.2a4659e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f45a18bcfd4d63388396",
+    "url": "/dashboard/static/js/main.13b229e9.chunk.js"
+  },
+  {
+    "revision": "97517561b618e80db8b2",
+    "url": "/dashboard/static/js/runtime-main.ca6bc846.js"
+  }
+]);
